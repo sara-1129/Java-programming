22bf49358a88c156b9b0e0a09984ac612088a3c7
Sara <sarasaini3116@gmail.com>
2025-03-09 14:58:47 +0530

    Learnt basic syntax

---
+++ JavaBasics/basics.java
@@ -0,0 +1,8 @@
+package JavaBasics;
+
+public class basics {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // If we do not use ln with system.out.println then it will not go to the next line
+        System.out.print("Hello, World!");
+    }
+}

e386aa516332704247c6939f9fe9df59e52059f7
Sara <sarasaini3116@gmail.com>
2025-03-10 18:34:25 +0530

    learnt about opertaors

---
+++ JavaBasics/operators.java
@@ -0,0 +1,49 @@
+package JavaBasics;
+
+public class operators {
+    public static void main (String args[]){
+
+        //operators
+
+        //arithmetic operators
+        double a = 1;
+        double b = 2;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+        
+        //assignment operators
+             //unary operators
+             int i = 1;
+             System.out.println(i++); //first uses then increments
+             System.out.println(i);
+
+             System.out.println(++i);//first increments then uses
+
+
+             //similarly decrement operators i-- , --i;
+
+        
+        //Comparision Operators  -->Return boolean values
+         
+            // a == b  
+            //a != b
+            //a < b
+            //a >= b
+
+
+
+        //logical operators
+            // &&
+            // ||
+            // !
+            
+
+        
+
+
+
+    }
+}

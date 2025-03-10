d7d2827ce126f93723e19d6e737a831856afb158
Sara <sarasaini3116@gmail.com>
2025-03-10 13:48:25 +0530

    Learnt about strings

---
+++ JavaBasics/strings.java
@@ -0,0 +1,31 @@
+package JavaBasics;
+
+public class strings {
+    public static void main(String[] args) {
+        // Strings
+
+        // concatenate
+        String name1 = "Sara";
+        String name2 = "Saini";
+        String name3 = name1 + " " + name2;
+        System.out.println(name3);
+
+        // charAt
+        System.out.println(name1.charAt(0));
+
+        // length
+        System.out.println(name1.length());
+
+        // replace
+        String name4 = name2.replace('a', 'b'); // It doesn't affect the original string
+        System.out.println(name4);
+        System.out.println(name2);
+
+        //substrings
+        String name5 ="Aman and Akku";
+        System.out.println(name5.substring(0,3)); //Index 3 will not be included
+        //Strings are immutable in java
+
+    }
+
+}

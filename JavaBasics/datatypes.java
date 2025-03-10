99cace6f205a235fcf46bcd8dfc3513d912d9ebd
Sara <sarasaini3116@gmail.com>
2025-03-10 14:09:28 +0530

    Modified

--- JavaBasics/datatypes.java
+++ JavaBasics/datatypes.java
@@ -13,12 +13,12 @@ public class datatypes {
         //double - 8
         //char - 2 a,b,c,d
         //boolean - 1(true/false)
-        byte age = 30;
-        int phone = 256357879;
-        long phone2 = 25657678698798L;
-        float pi = 3.14F;
-        char letter = 'a';
-        boolean adult = true;
+       // byte age = 30;
+       // int phone = 256357879;
+        // long phone2 = 25657678698798L;
+        // float pi = 3.14F;
+        // char letter = 'a';
+        // boolean adult = true;
 
 
         //non-primitive
--- JavaBasics/variables.java
+++ JavaBasics/variables.java
@@ -3,8 +3,9 @@ package JavaBasics;
 public class variables {
     public static void main(String[] args){
         String name ="Aman";
-        int age = 13;
-        String neighbour = name;
+        //int age = 13;
+         String neighbour = name;
+         System.out.println(neighbour);
         
 
     }

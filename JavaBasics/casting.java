f33240268b304793acad82769855f55d48132176
Sara <sarasaini3116@gmail.com>
2025-03-10 18:05:28 +0530

    Casting in java

---
+++ JavaBasics/casting.java
@@ -0,0 +1,26 @@
+package JavaBasics;
+
+public class casting {
+    public static void main(String[] args) {
+        //casting
+        double price = 100;
+        double finalPrice = price + 18; // Implicit Casting
+        System.out.println(finalPrice);
+
+         int p = 100;
+        //int fp = p + 18.0;  -->Error
+
+        int fp = p + (int)18.18; // Explicit casting
+        System.out.println(fp);
+
+
+        //constants
+        // int age = 30;
+        // age = 31;    -->Changes every year
+
+        final float PI = 3.14F;//In java we generally use uppercase letters to declare constants 
+        //PI =56.5  -->Error 
+        System.out.println(PI);
+
+    }
+}

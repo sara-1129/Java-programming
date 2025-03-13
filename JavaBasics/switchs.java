fb86e1376c61ae974a750098d5c5c0a1b5d6ea22
Sara <sarasaini3116@gmail.com>
2025-03-13 13:53:03 +0530

    Switch Case

---
+++ JavaBasics/switchs.java
@@ -0,0 +1,37 @@
+package JavaBasics;
+
+public class switchs {
+    public static void main(String[] args) {
+        //switch case
+        int day = 3;
+        //break is used to break the loop
+        //default is used when no case is matched.
+        
+        switch(day)
+        {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+    }
+}

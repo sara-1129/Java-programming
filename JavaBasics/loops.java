a15d7ed986291cfeeea34644c0635f698e5b15d2
Sara <sarasaini3116@gmail.com>
2025-03-13 14:13:01 +0530

    learnt about loops

---
+++ JavaBasics/loops.java
@@ -0,0 +1,27 @@
+package JavaBasics;
+
+public class loops {
+    public static void main(String[] args) {
+        // for loop
+        
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("for loop: " + i);
+        }
+
+        // while loop
+
+        int j = 1;
+        while (j <= 5) {
+            System.out.println("while loop: " + j);
+            j++;
+        }
+
+        // do-while loop
+
+        int k = 1;
+        do {
+            System.out.println("do-while loop: " + k);
+            k++;
+        } while (k <= 5);
+    }
+}

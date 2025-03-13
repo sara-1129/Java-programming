cd080a44794b48bc9309cef6e42b68fb65659fed
Sara <sarasaini3116@gmail.com>
2025-03-13 13:22:35 +0530

    learnt ifelse statement

---
+++ JavaBasics/ifelse.java
@@ -0,0 +1,23 @@
+package JavaBasics;
+
+public class ifelse {
+    public static void main(String[] args) {
+        //conditonal statements 
+        //ifelse
+        boolean isSunUp = true;
+        if (isSunUp) {
+            System.out.println("It is day.");
+        }
+        else {
+            System.out.println("It is night.");
+        }
+        int age = 30;
+        if(age >= 18)
+        {
+            System.out.println("Can Vote");
+        }
+        else{
+            System.out.println("Cannot Vote");
+        }
+    }
+}

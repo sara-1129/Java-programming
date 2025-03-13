5f5b2e21ffa88a652dbbcf36cdcb4bf3b99394bc
Sara <sarasaini3116@gmail.com>
2025-03-13 11:19:38 +0530

    Learnt about maths class

---
+++ JavaBasics/maths.java
@@ -0,0 +1,16 @@
+package JavaBasics;
+
+public class maths {
+    public static void main(String[] args) {
+        //Maths
+        //min
+        //max
+
+        System.out.println(Math.min(5,6));
+
+        //random 
+        System.out.println(Math.random()); //gives values from 0.1 to 0.999
+
+        
+    }
+}
